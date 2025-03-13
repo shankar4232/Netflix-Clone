c4df826c669dca5631454773ff53f9e857dff12b
Shankar4232 <shankarpadala4232@gmail.com>
2025-03-13 20:39:33 +0530

    Full Project

--- src/components/TVcards/Tvcards.jsx
+++ src/components/TVcards/Tvcards.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import "./TVCards.css";
+import "./Tvcards.css";
 
 const TVCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([]);
